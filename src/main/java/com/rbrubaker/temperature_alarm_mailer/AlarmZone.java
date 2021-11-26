c289fd6be144364e55d0742410c9f6fce1faf58c
JustBru00 <justbru00@gmail.com>
2021-11-26 14:42:22 -0500

    Added GPL-3.0 notice to AlarmZone, ConfigReader, and MailManager.

--- src/main/java/com/rbrubaker/temperature_alarm_mailer/AlarmZone.java
+++ src/main/java/com/rbrubaker/temperature_alarm_mailer/AlarmZone.java
@@ -5,7 +5,28 @@ import java.util.ArrayList;
 
 import io.prometheus.client.Gauge;
 
-
+/**
+* TemperatureAlarmMailer - A small program that monitors temperatures and sends alarm emails.
+*   Copyright (C) 2021 Rufus Brubaker Refrigeration
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>
+*   
+*   You can contact us at rbr@rbrubaker.com.
+* 
+* @author Justin Brubaker
+*
+*/
 public class AlarmZone {
 
 	private String name;
